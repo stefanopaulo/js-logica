453a6cd21b4217281269cc65bf0cb484326abbfb
Stefano Paulo <stefanopaulo47@gmail.com>
2020-08-27 14:23:17 -0300

    operação ternária

---
+++ aula007/script.js
@@ -0,0 +1,8 @@
+// operador ternário
+// condition ? true : false
+
+const punctuationUser = 1000;
+
+const levelUser = punctuationUser >= 1000 ? 'Usuário VIP' : 'Usuário normal';
+
+console.log(levelUser);
