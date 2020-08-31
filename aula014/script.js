72b82aae485d01be4293974fe0df5ed6ce10cd41
Stefano Paulo <stefanopaulo47@gmail.com>
2020-08-31 17:15:39 -0300

    for classico

---
+++ aula014/script.js
@@ -0,0 +1,10 @@
+console.log('Exibindo números pares entre 0 e 20');
+for (let i = 0; i <= 20; i += 2) {
+    console.log(i);
+}
+
+const fruit = ['Maçã', 'Uva', 'Pêra', 'Laranja', 'Melancia', 'Tangerina', 'Sapoti'];
+
+for (let i = 0; i < fruit.length; i++) {
+    console.log(fruit[i]);
+}
