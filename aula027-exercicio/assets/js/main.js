676e6ffddc9e15cd03564947014045df3b18f7e1
Stefano Paulo <stefanopaulo47@gmail.com>
2020-09-03 13:59:58 -0300

    refatorando exercicio setInterval

--- aula027-exercicio/assets/js/main.js
+++ aula027-exercicio/assets/js/main.js
@@ -1,45 +1,49 @@
-const selector = sel => document.querySelector(sel);
+function clock() {
+    const selector = sel => document.querySelector(sel);
 
-const clock = selector('.clock');
-const start = selector('.start');
-const pause = selector('.pause');
-const reset = selector('.reset');
-let seconds = 0;
-let timer;
+    const clock = selector('.clock');
+    const start = selector('.start');
+    const pause = selector('.pause');
+    const reset = selector('.reset');
+    let seconds = 0;
+    let timer;
 
-function getTimeFromSeconds(seconds) {
-    const date = new Date(seconds * 1000);
+    function getTimeFromSeconds(seconds) {
+        const date = new Date(seconds * 1000);
 
-    return date.toLocaleTimeString('pt-BR', {
-        hour12: false,
-        timeZone: 'UTC'
-    });
+        return date.toLocaleTimeString('pt-BR', {
+            hour12: false,
+            timeZone: 'UTC'
+        });
+    }
+
+
+    function startTimer() {
+        clearInterval(timer);
+        timer = setInterval(() => {
+            seconds++;
+            clock.innerHTML = getTimeFromSeconds(seconds);
+        }, 1000);
+        clock.style.color = "#000";
+    }
+
+    function pauseTimer() {
+        clearInterval(timer);
+        clock.style.color = "#f00";
+    }
+
+    function resetTimer() {
+        clearInterval(timer);
+        clock.innerHTML = '00:00:00';
+        seconds = 0;
+        clock.style.color = "#000";
+    }
+
+    start.addEventListener('click', startTimer);
+
+    reset.addEventListener('click', resetTimer);
+
+    pause.addEventListener('click', pauseTimer);
 }
 
-
-function startTimer() {
-    clearInterval(timer);
-    timer = setInterval(() => {
-        seconds++;
-        clock.innerHTML = getTimeFromSeconds(seconds);
-    }, 1000);
-    clock.style.color = "#000";
-}
-
-function pauseTimer() {
-    clearInterval(timer);
-    clock.style.color = "#f00";
-}
-
-function resetTimer() {
-    clearInterval(timer);
-    clock.innerHTML = '00:00:00';
-    seconds = 0;
-    clock.style.color = "#000";
-}
-
-start.addEventListener('click', startTimer);
-
-reset.addEventListener('click', resetTimer);
-
-pause.addEventListener('click', pauseTimer);
+clock();
